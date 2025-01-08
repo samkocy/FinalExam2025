46ed6f5a68e3351c61015047f0eb92d50da6345f
Samuel Kocych <S00255627@atu.ie>
2025-01-08 14:24:38 +0000

    methods in xaml.cs removed, code is no allocated directly in window loaded method

--- FinalExam2025/MainWindow.xaml.cs
+++ FinalExam2025/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 
 namespace FinalExam2025
@@ -29,6 +30,10 @@ namespace FinalExam2025
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Event> events = new List<Event>();
+        private List<Ticket> tickets = new List<Ticket>();
+        private List<VIPTicket> vips = new List<VIPTicket>();
+
         public MainWindow()
         {
             InitializeComponent(); 
@@ -37,49 +42,48 @@ namespace FinalExam2025
         // runs on window load and fills list box on the screen with events
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-           
-        }
-
-        // create a list of events
-        private List<Event> GetEvents()
-        {
-            Event e1 = new Event() { 
-                Name = "Oasis Croke Park", 
-                EventDate = new DateTime(2025, 06, 20), 
-                TypeOfEvent = EventType.Music 
+            // create a list of events
+            Event e1 = new Event()
+            {
+                Name = "Oasis Croke Park",
+                EventDate = new DateTime(2025, 06, 20),
+                TypeOfEvent = EventType.Music
             };
 
-            Event e2 = new Event() { 
-                Name = "Electric Picnic", 
-                EventDate = new DateTime(2025, 08, 20), 
-                TypeOfEvent = EventType.Music 
+            Event e2 = new Event()
+            {
+                Name = "Electric Picnic",
+                EventDate = new DateTime(2025, 08, 20),
+                TypeOfEvent = EventType.Music
             };
 
-            return new List<Event>() { e1, e2 };
-        }
+            // add to list
+            events.Add(e1);
+            events.Add(e2);
 
-        // create a list of tickets
-        private List<Ticket> GetTicket()
-        {
-            Ticket t1 = new Ticket() { 
-                Name = "Early Bird", 
-                Price = 100m, 
-                AvailableTickets = 100 
+
+            // create a list of tickets
+            Ticket t1 = new Ticket()
+            {
+                Name = "Early Bird",
+                Price = 100m,
+                AvailableTickets = 100
             };
 
-            Ticket t2 = new Ticket() { 
-                Name = "Platinium", 
-                Price = 150m, 
-                AvailableTickets = 100 
+            Ticket t2 = new Ticket()
+            {
+                Name = "Platinium",
+                Price = 150m,
+                AvailableTickets = 100
             };
 
-            return new List<Ticket>() { t1, t2 };
-        }
+            // add to list
+            tickets.Add(t1);
+            tickets.Add(t2);
 
-        // create a lis of VIP tickets
-        private List<VIPTicket> GetVIPTickets()
-        {
-            VIPTicket v1 = new VIPTicket() {
+            // create a alist of vip tickets
+            VIPTicket v1 = new VIPTicket()
+            {
                 Name = "Ticket and Hotel Package",
                 Price = 150m,
                 AdditionalCost = 100m,
@@ -87,7 +91,8 @@ namespace FinalExam2025
                 AvailableTickets = 100
             };
 
-            VIPTicket v2 = new VIPTicket() {
+            VIPTicket v2 = new VIPTicket()
+            {
                 Name = "Weekend Ticket",
                 Price = 200m,
                 AdditionalCost = 100m,
@@ -95,7 +100,26 @@ namespace FinalExam2025
                 AvailableTickets = 100
             };
 
-            return new List<VIPTicket>() { v1, v2 };
+            // add to list
+            vips.Add(v1);
+            vips.Add(v2);
+
+            // display
+            lbxEvents.ItemsSource = events;
+            lbxTickets.ItemsSource = tickets;
+        }
+
+
+        // method to reset and refresh the display
+        private void UpdateDisplay(List<Event> events, List<Ticket> tickets, List<VIPTicket> vips)
+        {
+            // reset the listboxes
+            lbxEvents.ItemsSource = null;
+            lbxTickets.ItemsSource = null;
+
+
+           
+
         }
     }
 }
